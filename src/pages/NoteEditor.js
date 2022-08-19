2d706b3fe75c5a619406c4a8e5d453635ed78e12
seul-dev <seul.dev@gmail.com>
2022-08-19 09:58:35 +0900

    feat: update 기능 구현

--- src/pages/NoteEditor.js
+++ src/pages/NoteEditor.js
@@ -4,7 +4,6 @@ import { useParams, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Button from '../components/Button';
 import Header from '../components/Header';
-import useFetch from './../util/useFetch';
 
 const NoteEditor = () => {
   const { id } = useParams();
@@ -15,16 +14,18 @@ const NoteEditor = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
-  //`http://localhost:3001/note/${id}`
-  // useEffect(() => {
-  //   fetch(`http://localhost:3001/note/${id}`)
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //     });
-  // }, [id]);
+  useEffect(() => {
+    if (id) {
+      fetch(`http://localhost:3001/note/${id}`)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setTitle(data.title);
+          setContent(data.content);
+        });
+    }
+  }, []);
 
   const handleSubmit = () => {
     if (title.length < 1) {
@@ -34,24 +35,44 @@ const NoteEditor = () => {
       contentRef.current.focus();
     }
     const timestamp = new Date().toLocaleString();
-
     const note = {
       title,
       content,
       created: timestamp,
     };
-    fetch(`http://localhost:3001/note/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(note),
-    }).then((res) => {
-      if (res.ok) {
-        navigate('/');
-        window.location.reload();
-      }
-    });
+
+    if (!id) {
+      fetch(`http://localhost:3001/note/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(note),
+      }).then((res) => {
+        if (res.ok) {
+          navigate('/');
+          window.location.reload();
+        }
+      });
+    } else {
+      // id값이 있을때는 put 요청
+      const updatedNote = {
+        title,
+        content,
+        created: timestamp,
+      };
+
+      fetch(`http://localhost:3001/note/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(updatedNote),
+      }).then((res) => {
+        if (res.ok) {
+          navigate('/');
+          window.location.reload();
+        }
+      });
+    }
   };
 
   const handleDelete = () => {
